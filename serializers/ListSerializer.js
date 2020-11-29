4290189b6a81a16d2d34b168b67195c9f26d37cc
James Perkins <jamesperkins@hey.com>
2020-11-29 08:12:41 -0500

    Update ListSerializer.js

--- serializers/ListSerializer.js
+++ serializers/ListSerializer.js
@@ -1,12 +1,22 @@
 import { Flex, List } from "@chakra-ui/core";
 
 const ListSerializer = (props) => {
-  const { node, children } = props;
-
-  return (
-    <Flex align="center" justify="center">
-      <List styleType="disc">{children}</List>
-    </Flex>
-  );
+  const { type, children } = props;
+  if (type === "bullet") {
+    return (
+      <Flex mb={8}>
+        <List styleType="disc">{children}</List>
+      </Flex>
+    );
+  } else {
+    return (
+      <Flex mb={8}>
+        <List as="ol" styleType="decimal">
+          {children}
+        </List>
+      </Flex>
+    );
+  }
 };
+
 export default ListSerializer;
